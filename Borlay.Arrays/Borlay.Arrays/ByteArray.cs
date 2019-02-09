af2dc0d22425afd006fa85a8bbc01efa967866d9
Loat <pauliukss@gmail.com>
2019-02-09 15:22:44 +0200

    done

---
+++ Borlay.Arrays/Borlay.Arrays/ByteArray.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Borlay.Arrays
+{
+    public class ByteArray : IEnumerable<byte>
+    {
+        private byte[] bytes = null;
+        private int hashCode;
+
+        public byte[] Bytes
+        {
+            get
+            {
+                if (bytes == null)
+                    throw new Exception("Not initialized");
+                return bytes;
+            }
+            set
+            {
+                if (bytes != null)
+                    throw new Exception("Cannot set bytes twice");
+
+                this.bytes = value ?? throw new ArgumentNullException(nameof(Bytes), "Cannot set null Byte array to ByteArray.Bytes");
+                this.hashCode = ComputeHash(bytes);
+            }
+        }
+
+        public bool IsSet => bytes != null;
+
+        public bool IsEmpty => bytes == null || bytes.Length == 0;
+
+        public ByteArray(byte[] bytes)
+        {
+            Bytes = bytes;
+        }
+
+        /// <summary>
+        /// For serialization only
+        /// </summary>
+        public ByteArray()
+        {
+        }
+
+        public byte this[int index]
+        {
+            get
+            {
+                return bytes[index];
+            }
+        }
+
+        public int Length => Bytes.Length;
+
+        public static ByteArray Create(byte[] bytes)
+        {
+            return new ByteArray(bytes);
+        }
+
+        public static bool operator ==(ByteArray left, ByteArray right)
+        {
+            if (left is null && right is null) return true;
+            if (left is null || right is null) return false;
+            if (left.hashCode != right.hashCode) return false;
+            return SequenceEqual(left.Bytes, right.Bytes);
+        }
+
+        public static bool operator !=(ByteArray left, ByteArray right)
+        {
+            if (left is null && right is null) return false;
+            if (left is null || right is null) return true;
+            if (left.hashCode != right.hashCode) return true;
+            return !SequenceEqual(left.Bytes, right.Bytes);
+        }
+
+        public static bool operator >(ByteArray left, ByteArray right)
+        {
+            if (left == right) return false;
+
+
+            var count = left.Length;
+            if (count > right.Length)
+                count = right.Length;
+
+            for (int i = 0; i < count; i++)
+            {
+                if (left[i] > right[i])
+                    return true;
+                else if (left[i] < right[i])
+                    return false;
+            }
+
+            return left.Length > right.Length;
+        }
+
+        public static bool operator <(ByteArray left, ByteArray right)
+        {
+            if (left == right) return false;
+
+
+            var count = left.Length;
+            if (count > right.Length)
+                count = right.Length;
+
+            for (int i = 0; i < count; i++)
+            {
+                if (left[i] < right[i])
+                    return true;
+                else if (left[i] > right[i])
+                    return false;
+            }
+
+            return left.Length < right.Length;
+        }
+
+        public static bool operator <=(ByteArray left, ByteArray right)
+        {
+            if (left == right) return true;
+            return left < right;
+        }
+
+        public static bool operator >=(ByteArray left, ByteArray right)
+        {
+            if (left == right) return true;
+            return left > right;
+        }
+
+        public static unsafe bool SequenceEqual(byte[] left, byte[] right)
+        {
+            if (left is null || right is null) return left == right;
+            int length = left.Length;
+            if (length != right.Length) return false;
+            fixed (byte* str = left)
+            {
+                byte* chPtr = str;
+                fixed (byte* str2 = right)
+                {
+                    byte* chPtr2 = str2;
+                    while (length >= 20)
+                    {
+                        if ((((*(((int*)chPtr)) != *(((int*)chPtr2))) ||
+                        (*(((int*)(chPtr + 4))) != *(((int*)(chPtr2 + 4))))) ||
+                        ((*(((int*)(chPtr + 8))) != *(((int*)(chPtr2 + 8)))) ||
+                        (*(((int*)(chPtr + 12))) != *(((int*)(chPtr2 + 12)))))) ||
+                        (*(((int*)(chPtr + 16))) != *(((int*)(chPtr2 + 16))))) break;
+
+                        chPtr += 20;
+                        chPtr2 += 20;
+                        length -= 20;
+                    }
+
+                    while (length >= 4)
+                    {
+                        if (*(((int*)chPtr)) != *(((int*)chPtr2))) break;
+                        chPtr += 4;
+                        chPtr2 += 4;
+
+                        length -= 4;
+                    }
+
+                    while (length > 0)
+                    {
+                        if (*chPtr != *chPtr2) break;
+                        chPtr++;
+                        chPtr2++;
+                        length--;
+                    }
+
+                    return (length <= 0);
+                }
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            var other = (ByteArray)obj;
+            if (other.hashCode != this.hashCode) return false;
+            return SequenceEqual(Bytes, other.Bytes);
+        }
+
+        public override int GetHashCode()
+        {
+            return hashCode;
+        }
+
+        public static int ComputeHash(byte[] data)
+        {
+            unchecked
+            {
+                var hash = 0;
+                for (var i = 0; i < data.Length; i += 4)
+                {
+                    hash = (hash << 4) + data[i];
+                }
+                return hash;
+            }
+        }
+
+        public static ByteArray New()
+        {
+            return New(32);
+        }
+
+        public static ByteArray New(int size)
+        {
+            var bytes = new byte[size];
+            var rnd = System.Security.Cryptography.RandomNumberGenerator.Create();
+            rnd.GetBytes(bytes);
+            var byteArray = new ByteArray(bytes);
+            return byteArray;
+        }
+
+        public IEnumerator<byte> GetEnumerator()
+        {
+            return ((IEnumerable<byte>)Bytes).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Bytes.GetEnumerator();
+        }
+    }
+}
---
+++ Borlay.Arrays/Borlay.Arrays/ByteArrayExtensions.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Linq;
+
+namespace Borlay.Arrays
+{
+    public static class ByteArrayExtensions
+    {
+        public static bool IsNullOrEmpty(this ByteArray byteArray)
+        {
+            return byteArray == null || byteArray.IsEmpty;
+        }
+
+        public static byte[] GetBytes(bool value)
+        {
+            return BitConverter.GetBytes(value);
+        }
+
+        public static byte[] GetBytes(ushort value)
+        {
+            return BitConverter.GetBytes(value).Endian();
+        }
+
+        public static byte[] GetBytes(short value)
+        {
+            return BitConverter.GetBytes(value).Endian();
+        }
+
+        public static byte[] GetBytes(uint value)
+        {
+            return BitConverter.GetBytes(value).Endian();
+        }
+
+        public static byte[] GetBytes(int value)
+        {
+            return BitConverter.GetBytes(value).Endian();
+        }
+
+        public static byte[] GetBytes(ulong value)
+        {
+            return BitConverter.GetBytes(value).Endian(); // todo test with big values
+        }
+
+        public static byte[] GetBytes(long value)
+        {
+            return BitConverter.GetBytes(value).Endian(); // todo test with big values
+        }
+
+
+        public static byte[] GetBytes<T>(T value)
+        {
+            var size = TypeSize.SizeOf<T>();
+            return GetBytes<T>(value, size);
+        }
+
+        public static byte[] GetBytes<T>(T value, int size)
+        {
+            T[] array = new T[] { value };
+
+            byte[] bytes = new byte[size];
+
+            Buffer.BlockCopy(array, 0, bytes, 0, size);
+            if (size > 1)
+                bytes.Endian();
+            return bytes;
+        }
+
+        public static void AddBytes(this byte[] bytes, ByteArray value, int index)
+        {
+            Array.Copy(value.Bytes, 0, bytes, index, value.Length);
+        }
+
+        public static void AddBytes(this byte[] bytes, ByteArray value, ref int index)
+        {
+            Array.Copy(value.Bytes, 0, bytes, index, value.Length);
+            index += value.Length;
+        }
+
+        public static void TryAddBytes(this byte[] bytes, ByteArray value, int index)
+        {
+            if (!value.IsNullOrEmpty())
+            {
+                Array.Copy(value.Bytes, 0, bytes, index, value.Length);
+            }
+        }
+
+        public static void TryAddBytes(this byte[] bytes, ByteArray value, ref int index)
+        {
+            if (!value.IsNullOrEmpty())
+            {
+                Array.Copy(value.Bytes, 0, bytes, index, value.Length);
+                index += value.Length;
+            }
+        }
+
+        public static void AddBytes(this byte[] bytes, byte[] bytesToAdd, ref int index)
+        {
+            Array.Copy(bytesToAdd, 0, bytes, index, bytesToAdd.Length);
+            index += bytesToAdd.Length;
+        }
+
+        public static void AddBytes<T>(this byte[] bytes, T value, int index)
+        {
+            var size = TypeSize.SizeOf<T>();
+            AddBytes<T>(bytes, value, size, ref index);
+        }
+
+        public static void AddBytes<T>(this byte[] bytes, T value, ref int index)
+        {
+            var size = TypeSize.SizeOf<T>();
+            AddBytes<T>(bytes, value, size, ref index);
+        }
+
+        public static void AddBytes<T>(this byte[] bytes, T value, int size, int index)
+        {
+            AddBytes<T>(bytes, value, size, ref index);
+        }
+
+        public static void AddBytes<T>(this byte[] bytes, T value, int size, ref int index)
+        {
+            T[] array = new T[] { value };
+
+            if (size > 1 && BitConverter.IsLittleEndian)
+            {
+                var buff = new byte[size];
+                Buffer.BlockCopy(array, 0, buff, 0, size);
+                Array.Reverse(buff);
+                Buffer.BlockCopy(buff, 0, bytes, index, size);
+            }
+            else
+            {
+                Buffer.BlockCopy(array, 0, bytes, index, size);
+            }
+
+            index += size;
+        }
+
+        public static bool ToBool(byte[] bytes)
+        {
+            var value = BitConverter.ToBoolean(bytes, 0);
+            return value;
+        }
+
+        public static ushort ToUShort(byte[] bytes)
+        {
+            var value = BitConverter.ToUInt16(bytes.Endian(), 0);
+            return value;
+        }
+
+        public static uint ToUInt(byte[] bytes)
+        {
+            var value = BitConverter.ToUInt32(bytes.Endian(), 0);
+            return value;
+        }
+
+        public static ulong ToULong(byte[] bytes)
+        {
+            var value = BitConverter.ToUInt64(bytes.Endian(), 0);
+            return value;
+        }
+
+        public static short ToShort(byte[] bytes)
+        {
+            var value = BitConverter.ToInt16(bytes.Endian(), 0);
+            return value;
+        }
+
+        public static int ToInt(byte[] bytes)
+        {
+            var value = BitConverter.ToInt32(bytes.Endian(), 0);
+            return value;
+        }
+
+        public static long ToLong(byte[] bytes)
+        {
+            var value = BitConverter.ToInt64(bytes.Endian(), 0);
+            return value;
+        }
+
+        public static T GetValue<T>(this byte[] bytes, int index)
+        {
+            var size = TypeSize.SizeOf<T>();
+            return GetValue<T>(bytes, size, ref index);
+        }
+
+        public static T GetValue<T>(this byte[] bytes, ref int index)
+        {
+            var size = TypeSize.SizeOf<T>();
+            return GetValue<T>(bytes, size, ref index);
+        }
+
+        public static T GetValue<T>(this byte[] bytes, int size, int index)
+        {
+            return GetValue<T>(bytes, size, ref index);
+        }
+
+        public static T GetValue<T>(this byte[] bytes, int size, ref int index)
+        {
+            T[] value = new T[1];
+
+            if (size > 1 && BitConverter.IsLittleEndian)
+            {
+                var buff = new byte[size];
+                Buffer.BlockCopy(bytes, index, buff, 0, size);
+                Array.Reverse(buff);
+                Buffer.BlockCopy(buff, 0, value, 0, size);
+            }
+            else
+            {
+                Buffer.BlockCopy(bytes, index, value, 0, size);
+            }
+
+            index += size;
+            return value[0];
+        }
+
+
+        public static byte[] Endian(this byte[] bytes)
+        {
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(bytes);
+            return bytes;
+        }
+
+        public static ByteArray ToByteArray(this byte[] bytes, ushort maxCount, ref ushort index)
+        {
+            if (bytes == null) return null;
+
+            var count = BitConverter.ToUInt16(bytes, index);
+            index += 2;
+            if (count > maxCount) throw new ArgumentOutOfRangeException(nameof(count), $"Max count of array is {maxCount} but was {count}");
+
+            var byteArray = ToByteArray(bytes, index, count);
+            index += count;
+            return byteArray;
+        }
+
+        public static ByteArray ToByteArrayExactly(this byte[] bytes, ushort mustCount, ref ushort index)
+        {
+            if (bytes == null) return null;
+
+            var count = BitConverter.ToUInt16(bytes, index);
+            index += 2;
+            if (count != mustCount) throw new ArgumentOutOfRangeException(nameof(count), $"Count of array must be {mustCount} but was {count}");
+
+            var byteArray = ToByteArray(bytes, index, count);
+            index += count;
+            return byteArray;
+        }
+
+        public static ByteArray ToByteArrayExactlyOrEmpty(this byte[] bytes, ushort mustCount, ref ushort index)
+        {
+            if (bytes == null) return null;
+
+            var count = BitConverter.ToUInt16(bytes, index);
+            index += 2;
+            if (count == 0) return null;
+            if (count != mustCount) throw new ArgumentOutOfRangeException(nameof(count), $"Count of array must be {mustCount} but was {count}");
+
+            var byteArray = ToByteArray(bytes, index, count);
+            index += count;
+            return byteArray;
+        }
+
+        public static ByteArray ToByteArray(this byte[] bytes, int start, int count)
+        {
+            if (bytes == null) return null;
+            if (count == 0) return null;
+
+            if ((bytes.Length - count) < start) throw new IndexOutOfRangeException("Byte array length is less than needed");
+
+            var array = new byte[count];
+            Array.Copy(bytes, start, array, 0, count);
+            var byteArray = new ByteArray(array);
+            return byteArray;
+        }
+
+        public static ByteArray ToByteArray(this byte[] bytes)
+        {
+            if (bytes == null) return null;
+            if (bytes.Length == 0) return null;
+            return new ByteArray(bytes);
+        }
+
+        public static byte[] ToArray(this ByteArray byteArray)
+        {
+            return byteArray.IsNullOrEmpty() ? null : byteArray.Bytes.ToArray();
+        }
+    }
+}
